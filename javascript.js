96e1668090cb5b861bf82dd4bfd3018cb0678d8b
Mantas1990 <brazionis.mantas@gmail.com>
2018-12-14 13:52:21 +0200

    Atnaujinta informacija

---
+++ javascript.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("goTop").style.display = "block";
+  } else {
+    document.getElementById("goTop").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
